a56a9c25e9b06fbbb9172b0f8d72f14cfc4cc5b0
pranish9 <pranishtuladhar9@gmail.com>
2025-10-14 11:31:06 +0545

    setup done

---
+++ TiffinSathi/src/main/java/com/tiffin_sathi/TiffinSathiApplication.java
@@ -0,0 +1,13 @@
+package com.tiffin_sathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiffinSathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiffinSathiApplication.class, args);
+	}
+
+}
---
+++ TiffinSathi/src/test/java/com/tiffin_sathi/TiffinSathiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiffin_sathi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiffinSathiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
