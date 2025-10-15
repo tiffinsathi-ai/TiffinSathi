3f057d45e664e64ca25e20ebb178594c0a4d6c8b
pranish9 <pranishtuladhar9@gmail.com>
2025-10-15 13:17:21 +0545

    helo

--- TiffinSathi/src/main/java/com/tiffin_sathi/TiffinSathiApplication.java
+++ TiffinSathi/src/main/java/com/tiffin_sathi/TiffinSathiApplication.java
@@ -2,12 +2,12 @@ package com.tiffin_sathi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TiffinSathiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TiffinSathiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TiffinSathiApplication.class, args);
+        System.out.println("hello world");
+    }
 }
